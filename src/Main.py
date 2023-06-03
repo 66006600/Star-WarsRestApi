ba06e0c3dd1ada3833c0d3f312cce775cdec99d0
Beverly M <bevponi@gmail.com>
2023-06-03 16:36:15 +0000

    Rev

---
+++ src/Main.py
@@ -0,0 +1,136 @@
+from flask import Flask, jsonify, request
+from flask_sqlalchemy import SQLAlchemy
+from models import User, Character, Planet, Favorite_Planet, Favorite_Character
+
+
+app = Flask(__name__)
+db = SQLAlchemy(app)
+
+
+@app.route('/User', methods=['GET'])
+def get_user():
+    json_text = jsonify(User)
+    return json_text
+
+
+@app.route('/favorite/planet/<int:planet_id>', methods=['POST', 'DELETE'])
+def manage_favorite_planet(planet_id):
+    if request.method == 'POST':
+
+        if Favorite_Planet.query.filter_by(user_id=1, planet_id=planet_id).first():
+            return jsonify({'message': 'El planeta ya es un favorito'}), 409
+
+        favorite_planet = Favorite_Planet(user_id=1, planet_id=planet_id)
+        db.session.add(favorite_planet)
+        db.session.commit()
+
+        return jsonify({'message': 'Planeta agregado como favorito'}), 201
+
+    elif request.method == 'DELETE':
+
+        # favorite_planet = Favorite_Planet.query.filter_by(
+        #     user_id=User.id, planet_id=planet_id).first()
+
+        if favorite_planet:
+            db.session.delete(favorite_planet)
+            db.session.commit()
+            return jsonify({'message': 'Planeta eliminado de favoritos'}), 200
+        else:
+            return jsonify({'message': 'El planeta no es un favorito'}), 404
+
+    return jsonify({'message': 'Método inválido'}), 405
+
+
+@app.route('/favorite/character/<int:character_id>', methods=['POST', 'DELETE'])
+def manage_favorite_character(character_id):
+    if request.method == 'POST':
+
+        if Favorite_Character.query.filter_by(user_id=1, character_id=character_id).first():
+            return jsonify({'message': 'El personaje ya es tu favorito'}), 409
+
+        favorite_character = Favorite_Character(
+            user_id=1, character_id=character_id)
+        db.session.add(favorite_character)
+        db.session.commit()
+
+        return jsonify({'message': 'Personaje agregado como favorito'}), 201
+
+    elif request.method == 'DELETE':
+
+        # favorite_character = Favorite_Character.query.filter_by(
+        #     user_id=user.id, character_id=character_id).first()
+
+        if favorite_character:
+            db.session.delete(favorite_character)
+            db.session.commit()
+            return jsonify({'message': 'Personaje eliminado de favoritos'}), 200
+        else:
+            return jsonify({'message': 'Este personaje ya no es un favorito'}), 404
+
+    return jsonify({'message': 'Método inválido'}), 405
+
+
+@app.route('/planets', methods=['GET', 'PUT', 'POST'])
+def get_planets():
+    if request.method == 'GET':
+        planets = Planet.query.all()
+        return jsonify(planet.serialize() for planet in planets), 200
+
+    elif request.method == 'PUT':
+        planet_id = request.json.get('planet_id')
+        planet_name = request.json.get('planet_name')
+
+        planet = Planet.query.get(planet_id)
+
+        if not planet:
+            return jsonify({'message': 'El planeta no existe'}), 404
+
+        planet.name = planet_name
+        db.session.commit()
+        return jsonify(planet.serialize()), 200
+
+    elif request.method == 'POST':
+        planet_name = request.json.get('planet_name')
+
+        new_planet = Planet(name=planet_name)
+        db.session.add(new_planet)
+        db.session.commit()
+        return jsonify({'message': 'Planeta agregado correctamente', 'planet': new_planet.serialize()}), 201
+
+    else:
+        return jsonify({'message': 'Invalid method'}), 405
+
+
+@app.route('/characters', methods=['GET', 'PUT', 'POST'])
+def get_characters():
+
+        if request.method == 'GET':
+            planets = Character.query.all()
+            return jsonify(Character.serialize()), 200
+
+        elif request.method == 'PUT':
+            character_id = request.json.get('character_id')
+            character_name = request.json.get('character_name')
+
+            character = Character.query.get(character_id)
+
+            if not character:
+                return jsonify({'message': 'El personaje no existe'}), 404
+
+            character.name = character_name
+            db.session.commit()
+            return jsonify(character.serialize()), 200
+
+        elif request.method == 'POST':
+
+            character_name = request.json.get('character_name')
+
+            new_character = Character(name=character_name)
+            db.session.add(new_character)
+            db.session.commit()
+            return jsonify({'message': 'Personaje agregado correctamente'}), 201
+
+        else:
+            return jsonify({'message': 'Invalid method'}), 405
+        
+        
--- src/models.py
+++ src/models.py
@@ -1,12 +1,55 @@
+from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import ForeignKey
+from sqlalchemy import String, Integer
 
+
+app = Flask(__name__)
 db = SQLAlchemy()
 
 class User(db.Model):
+    __tablename__ = 'users'
+    
     id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(30), nullable=False)
+    last_name = db.Column(db.String(30), nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(80), unique=False, nullable=False)
     is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+    personajes = db.relationship('Favorite_Character', backref='usuario')
+    planetas = db.relationship('Favorite_Planet', backref='usuario')
+    
+
+class Character(db.Model):
+    __tablename__ = 'characters'
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    height = db.Column(db.String(80), unique=False, nullable=False)
+    is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+    favorits = db.relationship('FavoriteCharacter', backref='personajes')
+
+class Planet(db.Model):
+    __tablename__ = 'planets'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(String(50), nullable=False)
+    poblacion = db.Column(String(50), nullable=False)   
+    favoritos = db.relationship('FavoritePlanet', backref='planetas')
+
+class Favorite_Planet(db.Model):
+    __tablename__ = 'favorite_planet'
+    user_id = db.Column(Integer, ForeignKey('user.id'), primary_key = True)
+    planet_id = db.Column(Integer, ForeignKey('planet.id'), primary_key = True)
+
+    users = db.relationship(User, back_populates="favorite_planet")
+
+   
+class Favorite_Character(db.Model):
+    __tablename__ = 'favorite_character'   
+    user_id = db.Column(Integer, ForeignKey('user.id'), primary_key = True)
+    character_id = db.Column(Integer, ForeignKey('character.id'), primary_key = True)
+
+    users = db.relationship(User, back_populates="favorite_character")
+
 
     def __repr__(self):
         return '<User %r>' % self.username
